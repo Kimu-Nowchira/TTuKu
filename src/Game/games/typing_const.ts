1c45ea7bda047cbbf3a599c279a875498753e046
Kimu-Nowchira <hon20ke@naver.com>
2022-11-06 21:57:29 +0900

    refactor: typing_const.ts

--- src/Game/games/typing_const.ts
+++ src/Game/games/typing_const.ts
@@ -1,35 +1,35 @@
 /**
  * Rule the words! KKuTu Online
  * Copyright (C) 2017 JJoriping(op@jjo.kr)
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-﻿var File = require('fs');
+import { readFile } from "fs"
 
-exports.PROVERBS = {
-	'ko': [],
-	'en': []
-};
+export const PROVERBS = {
+  ko: [] as string[],
+  en: [] as string[],
+}
 
-File.readFile(`${__dirname}/../../data/proverbs.txt`, function(err, res){
-	if(err) throw Error(err.toString());
-	var db = res.toString().split('~~~');
-	
-	db.forEach(function(item){
-		var lang = item.slice(0, 2);
-		
-		exports.PROVERBS[lang] = item.slice(3).split('\n');	
-	});
-});
+readFile(`${__dirname}/../../data/proverbs.txt`, (err, res) => {
+  if (err) throw Error(err.toString())
+  const db = res.toString().split("~~~")
+
+  db.forEach((item) => {
+    const lang = item.slice(0, 2) as keyof typeof PROVERBS
+
+    PROVERBS[lang] = item.slice(3).split("\n")
+  })
+})
