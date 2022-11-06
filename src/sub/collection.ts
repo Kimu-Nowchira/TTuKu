41617680d4f689500a80d9ffb5595e0d9bc46f2d
Kimu-Nowchira <hon20ke@naver.com>
2022-11-07 02:13:23 +0900

    chore: remove debug logging

--- src/sub/collection.ts
+++ src/sub/collection.ts
@@ -309,7 +309,6 @@ class Pointer {
     }
 
     const preCB = (err: Error, res) => {
-      logger.debug(err, res)
       if (err) {
         logger.error("Error when querying: " + sql)
         logger.error("Context: " + err.toString())
