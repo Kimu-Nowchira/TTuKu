d9d70efc72c77590dfe3739c7c981298704a6edb
Kimu-Nowchira <hon20ke@naver.com>
2022-11-06 22:17:17 +0900

    chore: 코드 정리

--- src/sub/recaptcha.ts
+++ src/sub/recaptcha.ts
@@ -1,12 +1,12 @@
+import { config } from "../config"
 const request = require("request")
-const GLOBAL = require("./global.json")
 
-exports.verifyRecaptcha = function (
+export const verifyRecaptcha = (
   responseToken: string,
   remoteIp: string,
   callback: (success: boolean) => void
-) {
-  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${GLOBAL.GOOGLE_RECAPTCHA_SECRET_KEY}&response=${responseToken}&remoteip=${remoteIp}`
+) => {
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${config.GOOGLE_RECAPTCHA_SECRET_KEY}&response=${responseToken}&remoteip=${remoteIp}`
   request(verifyUrl, (err: Error, _response: any, body: any) => {
     try {
       const responseBody = JSON.parse(body)
