1973f76ceab92d361f4eb85ae34c55215e5839f5
Kimu-Nowchira <hon20ke@naver.com>
2022-11-11 22:03:01 +0900

    chore: 코드 구조 및 타입 개선

--- src/Game/classes/Data.ts
+++ src/Game/classes/Data.ts
@@ -5,33 +5,41 @@ export default class Data {
   score: number
   playTime: number
   connectDate: number
-  record: Record<string, any>
+  record: Record<string, any> = {}
 
-  constructor(data?: {
-    score: number
-    playTime: number
-    connectDate: number
-    record: Record<string, number[]>
-  }) {
-    if (!data)
-      data = {
-        score: 0,
-        playTime: 0,
-        connectDate: 0,
-        record: {},
-      }
-
+  constructor(
+    data: {
+      score: number
+      playTime: number
+      connectDate: number
+      record: Record<string, [number, number, number, number]>
+    } = {
+      score: 0,
+      playTime: 0,
+      connectDate: 0,
+      record: {},
+    }
+  ) {
     this.score = data.score || 0
     this.playTime = data.playTime || 0
     this.connectDate = data.connectDate || 0
-    this.record = {}
-    for (const i in GAME_TYPE) {
-      const j = GAME_TYPE[i]
-      this.record[j] = data.record
-        ? data.record[GAME_TYPE[i]] || [0, 0, 0, 0]
-        : [0, 0, 0, 0]
-      if (!this.record[j][3]) this.record[j][3] = 0
+
+    for (const gameType of GAME_TYPE) {
+      // 전, 승, 점수
+      this.record[gameType] = [0, 0, 0, 0]
+
+      if (data.record && data.record[gameType])
+        this.record[gameType] = data.record[gameType]
+
+      if (!this.record[gameType][3]) this.record[gameType][3] = 0
     }
+
+    // for (const i in GAME_TYPE) {
+    //   const j = GAME_TYPE[i]
+    //   this.record[j] = data.record
+    //     ? data.record[GAME_TYPE[i]] || [0, 0, 0, 0]
+    //     : [0, 0, 0, 0]
+    //   if (!this.record[j][3]) this.record[j][3] = 0
+    // }
   }
-  // 전, 승, 점수
 }
