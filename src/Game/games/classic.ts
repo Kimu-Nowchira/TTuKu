1effca7717447ef13924de8927270c64896df670
Kimu-Nowchira <hon20ke@naver.com>
2022-11-12 03:39:13 +0900

    fix: apply 제거

--- src/Game/games/classic.ts
+++ src/Game/games/classic.ts
@@ -617,8 +617,8 @@ function getAuto(char: string, subc: string, type: 0 | 1 | 2): Tail {
         }
         break
     }
-    this.DB.kkutu[this.room.rule.lang].find
-      .apply(this, aqs)
+    this.DB.kkutu[this.room.rule.lang]
+      .find(aqs)
       .limit(bool ? 1 : 123)
       .on(($md) => {
         forManner($md)
