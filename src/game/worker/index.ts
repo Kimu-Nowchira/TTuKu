669ce7f8d1b9014d6220500367d373f45f994265
Kimu-Nowchira <keleekini@gmail.com>
2022-11-16 14:39:03 +0900

    fix: 이전 커밋으로 복원

--- src/game/worker/index.ts
+++ src/game/worker/index.ts
@@ -26,14 +26,19 @@ import process from "node:process"
 import WebSocket from "ws"
 import { logger } from "../../sub/jjlog"
 import Room from "../classes/Room"
-import { DEVELOP, GUEST_PERMISSION } from "../master"
+import {
+  DEVELOP,
+  ENABLE_ROUND_TIME,
+  MODE_LENGTH,
+  GUEST_PERMISSION,
+  ENABLE_FORM,
+} from "../master"
 import { config } from "../../config"
 import { init as dbInit, ip_block, session } from "../../sub/db"
 import { z } from "zod"
 import { IPBlockData, ISession, roomDataSchema, RoomDataToSend } from "../types"
 import "./processEvent"
 import { onClientMessageOnSlave } from "./clientEvent"
-import { IncomingMessage } from "http"
 
 let Server: WebSocket.Server
 let HTTPS_Server
@@ -56,8 +61,8 @@ export const reservedRoomCache: Record<
 
 export const run = async () => {
   await dbInit()
-  logger.info("DB is ready (SLAVE)")
 
+  logger.info("DB is ready (SLAVE)")
   KKuTuInit(workerClientData, workerRoomData, GUEST_PERMISSION, undefined, {
     onClientClosed: onClientClosedOnSlave,
     onClientMessage: onClientMessageOnSlave,
@@ -76,132 +81,121 @@ export const run = async () => {
     })
   }
 
-  logger.debug("프로미스 시작")
-  const { socket, info } = await new Promise<{
-    socket: WebSocket
-    info: IncomingMessage
-  }>((res, rej) => {
-    Server.on("connection", (socket, info) => {
-      logger.debug("커넥션")
-      socket.on("error", (err) => {
-        logger.warn("Error on #" + key + " on ws: " + err.toString())
-      })
+  // 플레이어가 소켓에 연결된 경우
+  Server.on("connection", async (socket, info) => {
+    if (!info.url) throw new Error("No URL on IncomingMessage")
 
-      res({ socket, info })
+    socket.on("error", (err) => {
+      logger.warn("Error on #" + key + " on ws: " + err.toString())
     })
 
-    Server.on("error", (err) => {
-      logger.error("Error on ws: " + err.toString())
-      rej(err)
-    })
-  })
-  logger.debug("프로미스 종료", socket, info)
+    const chunk = info.url.slice(1).split("&")
+    const key = chunk[0]
+    const reserve = reservedRoomCache[key]
 
-  if (!info.url) throw new Error("No URL on IncomingMessage")
-  const chunk = info.url.slice(1).split("&")
-  const key = chunk[0]
-  const reserve = reservedRoomCache[key]
+    if (channelId !== Number(chunk[1])) {
+      logger.warn(`Wrong channel value ${chunk[1]} on @${channelId}`)
+      socket.close()
+      return
+    }
 
-  if (channelId !== Number(chunk[1])) {
-    logger.warn(`Wrong channel value ${chunk[1]} on @${channelId}`)
-    socket.close()
-    return
-  }
+    const _room = reserve.room
 
-  if (!reserve.room) {
-    logger.error(`Not reserved from ${key} on @${channelId}`)
-    return socket.close()
-  }
+    if (!reserve.room) {
+      logger.error(`Not reserved from ${key} on @${channelId}`)
+      return socket.close()
+    }
 
-  const _room = reserve.room
-  const room: RoomDataToSend = {
-    ..._room,
-    id: _room._create ? undefined : _room.id,
-    _id: _room._create ? _room.id : undefined,
-  }
+    const room: RoomDataToSend = {
+      ..._room,
+      id: _room._create ? undefined : _room.id,
+      _id: _room._create ? _room.id : undefined,
+    }
 
-  clearTimeout(reserve._expiration)
-  delete reserve._expiration
-  delete reservedRoomCache[key]
+    clearTimeout(reserve._expiration)
+    delete reserve._expiration
+    delete reservedRoomCache[key]
 
-  const $body = await session
-    .findOne(["_id", key])
-    .limit(["profile", true])
-    .onAsync<ISession | undefined>()
+    const $body = await session
+      .findOne(["_id", key])
+      .limit(["profile", true])
+      .onAsync<ISession | undefined>()
 
-  const $c = new Client(socket, $body ? $body.profile : null, key)
-  $c.admin = config.ADMIN.includes($c.id)
+    const $c = new Client(socket, $body ? $body.profile : null, key)
+    $c.admin = config.ADMIN.includes($c.id)
 
-  // IP 차단 여부 확인
-  if (!info.connection.remoteAddress)
-    throw new Error("No remoteAddress on IncomingMessage")
+    // IP 차단 여부 확인
+    $c.remoteAddress = config.USER_BLOCK_OPTIONS.USE_X_FORWARDED_FOR
+      ? info.connection.remoteAddress
+      : (info.headers["x-forwarded-for"] as string) ||
+        info.connection.remoteAddress
 
-  $c.remoteAddress = config.USER_BLOCK_OPTIONS.USE_X_FORWARDED_FOR
-    ? info.connection.remoteAddress
-    : (info.headers["x-forwarded-for"] as string) ||
-      info.connection.remoteAddress
+    if (
+      config.USER_BLOCK_OPTIONS.USE_MODULE &&
+      ((config.USER_BLOCK_OPTIONS.BLOCK_IP_ONLY_FOR_GUEST && $c.guest) ||
+        !config.USER_BLOCK_OPTIONS.BLOCK_IP_ONLY_FOR_GUEST)
+    ) {
+      const ipBlockData = await ip_block
+        .findOne(["_id", $c.remoteAddress])
+        .onAsync<IPBlockData | undefined>()
 
-  if (
-    config.USER_BLOCK_OPTIONS.USE_MODULE &&
-    ((config.USER_BLOCK_OPTIONS.BLOCK_IP_ONLY_FOR_GUEST && $c.guest) ||
-      !config.USER_BLOCK_OPTIONS.BLOCK_IP_ONLY_FOR_GUEST)
-  ) {
-    const ipBlockData = await ip_block
-      .findOne(["_id", $c.remoteAddress])
-      .onAsync<IPBlockData | undefined>()
+      if (ipBlockData && ipBlockData.reasonBlocked) {
+        $c.socket.send(
+          JSON.stringify({
+            type: "error",
+            code: 446,
+            reasonBlocked: !ipBlockData.reasonBlocked
+              ? config.USER_BLOCK_OPTIONS.DEFAULT_BLOCKED_TEXT
+              : ipBlockData.reasonBlocked,
+            ipBlockedUntil: !ipBlockData.ipBlockedUntil
+              ? config.USER_BLOCK_OPTIONS.BLOCKED_FOREVER
+              : ipBlockData.ipBlockedUntil,
+          })
+        )
+        $c.socket.close()
+        return
+      }
+    }
 
-    if (ipBlockData && ipBlockData.reasonBlocked) {
-      $c.socket.send(
-        JSON.stringify({
-          type: "error",
-          code: 446,
-          reasonBlocked: !ipBlockData.reasonBlocked
-            ? config.USER_BLOCK_OPTIONS.DEFAULT_BLOCKED_TEXT
-            : ipBlockData.reasonBlocked,
-          ipBlockedUntil: !ipBlockData.ipBlockedUntil
-            ? config.USER_BLOCK_OPTIONS.BLOCKED_FOREVER
-            : ipBlockData.ipBlockedUntil,
-        })
-      )
+    // 이미 게임에 접속해 있는 경우
+    if (workerClientData[$c.id]) {
+      workerClientData[$c.id].send("error", { code: 408 })
+      workerClientData[$c.id].socket.close()
+    }
+
+    // 서버 점검 중인데 지정된 테스터 유저가 아닌 경우
+    if (DEVELOP && !TESTER.includes($c.id)) {
+      $c.send("error", { code: 500 })
       $c.socket.close()
       return
     }
-  }
 
-  // 이미 게임에 접속해 있는 경우
-  if (workerClientData[$c.id]) {
-    workerClientData[$c.id].send("error", { code: 408 })
-    workerClientData[$c.id].socket.close()
-  }
+    const ref = await $c.refresh()
 
-  // 서버 점검 중인데 지정된 테스터 유저가 아닌 경우
-  if (DEVELOP && !TESTER.includes($c.id)) {
-    $c.send("error", { code: 500 })
-    $c.socket.close()
-    return
-  }
+    if (ref.result == 200) {
+      workerClientData[$c.id] = $c
+      DNAME[($c.profile.title || $c.profile.name).replace(/\s/g, "")] = $c.id
 
-  const ref = await $c.refresh()
+      $c.enter(room, reserve.spec, reserve.pass)
 
-  if (ref.result == 200) {
-    workerClientData[$c.id] = $c
-    DNAME[($c.profile.title || $c.profile.name).replace(/\s/g, "")] = $c.id
+      if ($c.place == room.id) $c.publish("connRoom", { user: $c.getData() })
+      // 입장 실패
+      else $c.socket.close()
 
-    $c.enter(room, reserve.spec, reserve.pass)
+      logger.info(`Chan @${channelId} New #${$c.id}`)
+    } else {
+      $c.send("error", {
+        code: ref.result,
+        message: ref.black,
+      })
+      $c._error = ref.result
+      $c.socket.close()
+    }
+  })
 
-    if ($c.place == room.id) $c.publish("connRoom", { user: $c.getData() })
-    // 입장 실패
-    else $c.socket.close()
-
-    logger.info(`Chan @${channelId} New #${$c.id}`)
-  } else {
-    $c.send("error", {
-      code: ref.result,
-      message: ref.black,
-    })
-    $c._error = ref.result
-    $c.socket.close()
-  }
+  Server.on("error", (err) => {
+    logger.warn("Error on ws: " + err.toString())
+  })
 
   logger.info(`<< KKuTu Server:${Server.options.port} >>`)
 }
